fa9305da7a74ccac604e86a98ec95f3eef929c78
kbdoroja <kbdoroja@up.edu.ph>
2025-02-19 18:11:29 +0800

    Feat: succesfully implemented the validate password and reverse password

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 function validatePassword(x, y){
     var hasNumber = /\d/;   
-    if(string1 == string2 && string1.length >= 8 && hasNumber.test(string1)){
-        console.log("true"); 
-        return true; 
-    }else{
-       return false; 
+    if(x == y && x.length >= 8 && hasNumber.test(x)){
+       if(checkUpper(x) == true && checkLower(x) == true){return true;} 
     }
+    return false; 
+}
+
+function checkLower(string){
+    for(let a = 0; a < string.length; a++){
+        if(string.charCodeAt(a) >= 97 && string.charCodeAt(a) <= 122){
+            return true; 
+        }
+    }
+    return false; 
 }
 
 function checkUpper(string){
     for(let a = 0; a < string1.length; a++){
-        if(string[a] == string[a].toLowerCase){
+        if(string.charCodeAt(a) >= 65 && string.charCodeAt(a) <= 90){
             return true; 
         }
     }
+    return false; 
 }
-
 function reverse(string){
     var newarray = [];
     for(let x = (string.length - 1); x > -1; x-- ){
@@ -25,12 +32,18 @@ function reverse(string){
     var wow = newarray.join(""); 
     return wow; 
 }
-var string1 = "password123"; 
-var string2 = "password123"; 
+
+var string1 = "Password123"; 
+var string2 = "Password123"; 
 
 
 var validation = validatePassword(string1, string2); 
 console.log(validation); 
+
+console.log(checkLower(string1)); 
+console.log(checkUpper(string1));
+
+
 reverse(string1);
 
 
