458f30421c26a4ea55cf35fbe073d68b40a46c0e
kbdoroja <kbdoroja@up.edu.ph>
2025-02-19 18:23:21 +0800

    Succesfully implemented the storePassword function

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ function checkLower(string){
 }
 
 function checkUpper(string){
-    for(let a = 0; a < string1.length; a++){
+    for(let a = 0; a < string.length; a++){
         if(string.charCodeAt(a) >= 65 && string.charCodeAt(a) <= 90){
             return true; 
         }
     }
     return false; 
 }
+
 function reverse(string){
     var newarray = [];
     for(let x = (string.length - 1); x > -1; x-- ){
@@ -33,18 +34,20 @@ function reverse(string){
     return wow; 
 }
 
-var string1 = "Password123"; 
-var string2 = "Password123"; 
+function storePassword(user, string1, string2){
+    if(validatePassword(string1, string2) == true){
+        let a = reverse(string1); 
+        let object = {name: user, newpassword: a}
+        return object; 
+    }else{
+        let object = {name: user, newpassword: string1}; 
+        return object; 
+    }
+}
 
 
-var validation = validatePassword(string1, string2); 
-console.log(validation); 
-
-console.log(checkLower(string1)); 
-console.log(checkUpper(string1));
-
-
-reverse(string1);
+console.log(storePassword("John", "Pass1234", "Pass1234"));  // returns {name: "John", newpassword:"4321ssaP"}
+console.log(storePassword("John", "Pass123", "Pass12345")); // returns {name: "John", newpassword:"Pass123"}
 
 
 
